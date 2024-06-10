551b0bc93e91ff0be7f3be863a548f35157cf05b
Parkchaeyeon <ccyy1029@naver.com>
2024-06-10 22:03:02 +0900

    🎉 init: 함수 실행하는 부분 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    const randomNumber = MissionUtils.Random.pickUniqueNumbersIsRange(1, 10, 3);
+    
+
+
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
